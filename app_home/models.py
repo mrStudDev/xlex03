8fdef4f9932404c2e3790bed02b1a35c4b366090
mrStudDev <marcosscfloripa@gmail.com>
2024-04-12 11:49:17 -0300

    Indexable

--- app_home/models.py
+++ app_home/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils.text import slugify
 
 class TagSiteModel(models.Model):
     name = models.CharField(max_length=255)
@@ -12,17 +12,24 @@ class TagSiteModel(models.Model):
 
 class HomeSite(models.Model):
     site_name = models.CharField(max_length=255)
-    site_description = models.CharField(max_length=500)
+    site_description = models.TextField()  # Alterado para TextField
+    meta_title = models.CharField(max_length=255, default="Default Meta Title")
+    meta_description = models.TextField(max_length=320)  # Capacidade aumentada
     founder = models.CharField(max_length=100)
-    meta_description = models.TextField(max_length=255)
     keyword = models.CharField(max_length=255)
     tags = models.ManyToManyField('TagSiteModel', blank=True)
-    date_created = models.DateField(auto_now=False, auto_now_add=True)
+    date_created = models.DateField(auto_now_add=True)
     is_published = models.BooleanField(default=True)
     old_url = models.SlugField(blank=True, null=True)
     slug = models.SlugField(blank=True, unique=True)
     views = models.IntegerField(default=0)
-    
+    indexable = models.BooleanField(default=True)  # Novo campo para SEO
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.site_name)
+        super(HomeSite, self).save(*args, **kwargs)
+
     def __str__(self):
         return f"{self.site_name} | {self.founder} | {self.views}"
     
